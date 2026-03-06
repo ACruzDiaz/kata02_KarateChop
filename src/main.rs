caafc242d19875c9378ebe862ed9047aa8d3bd04
alan cruz <cruza86@uabc.edu.mx>
2026-03-06 12:59:46 -0800

    imperative binary search

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,38 @@ fn imperative_bs(arr: &[isize], target: isize) -> isize {
     -1
 }
 
+#[allow(dead_code)]
+fn recursive_bs(arr: &[isize], target: isize) -> isize {
+    if arr.is_empty() {
+        return -1;
+    }
+
+    let center = arr.len() as isize / 2;
+    let mid = arr[center as usize];
+
+    if mid == target {
+        center
+    } else if mid > target {
+        recursive_bs(
+            &arr[0..center as usize], 
+            target)
+    } else {
+        let result = recursive_bs(
+            &arr[(center as usize + 1)..], 
+            target);
+        if result == -1 {
+            -1
+        } else {
+            result + center + 1
+        }
+    }
+}
+
 
 
 #[cfg(test)]
 mod tests {
-    use super::imperative_bs;
+    use super::*;
 
     #[test]
     fn test_imperative_bs_found() {
@@ -39,5 +66,17 @@ mod tests {
     fn test_imperative_bs_not_found() {
         let test_arr = [1, 2, 3, 4, 5, 6, 7];
         assert_eq!(imperative_bs(&test_arr, 9), -1);
+    }
+
+        #[test]
+    fn test_recursive_bs_found() {
+        let test_arr = [1, 2, 3, 4, 5, 6, 7, 8];
+        assert_eq!(recursive_bs(&test_arr, 1), 0);
+    }
+
+    #[test]
+    fn test_recursive_bs_not_found() {
+        let test_arr = [1, 2, 3, 4, 5, 6, 7];
+        assert_eq!(recursive_bs(&test_arr, 9), -1);
     }
 }
